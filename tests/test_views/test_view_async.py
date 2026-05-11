f6564018d7f9f72f18959431d27d1473b3052aa7
Miradil Zeynalli <miradil.zeynalli@gmail.com>
2026-05-12 00:19:02 +0200

    Fixed overriding form_args in forms.py and widgets.py
    I stumbled upon this when I wanted to have `allow_blank` to be `True` in
    one of the fields. However, this was not working:
    
    ```python
    form_args = {
        "personnel": {
            "allow_blank": True,
            "blank_text": "-- Choose --",
        }
    }
    ```
    
    Because, in sqladmin/forms.py:218:
    
    `kwargs["allow_blank"] = nullable` overrode my param. So, I changed this
    (and all others) to use `setdefault`: only put values if user has not
    defined anything.

--- tests/test_views/test_view_async.py
+++ tests/test_views/test_view_async.py
@@ -183,6 +183,11 @@ class UserAdmin(ModelView, model=User):
         ],
         User.profile_formattable: lambda m, a: f"Formatted {m.profile_formattable}",
     }
+    form_args = {
+        "profile": {
+            "allow_blank": True,
+        },
+    }
     save_as = True
 
 
